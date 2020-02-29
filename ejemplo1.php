68682eab25b3ab2d5b0703eb5e45f21cffae58c4
precianter <40182626+precianter@users.noreply.github.com>
2020-02-28 21:14:21 -0600

    Add files via upload
    Ejemplo variables PHP

---
+++ ejemplo1.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>PW1: Ejemplo 1</title>
+    </head>
+    <body>
+        <?php
+        $num1 = 87; //declaracion de variable con valor entero
+        $num2 = 22.13; //declaracion de variable con valor decimal
+        $sum = $num1 + $num2;
+        echo $sum; //imprimir la variable Suma
+        ?>
+    </body>
+</html>
